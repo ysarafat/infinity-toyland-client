ab322d3779498e31f4a76f52027cc144493f500d
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-18 22:59:43 +0600

    add all toy page and slice data

--- src/Pages/AllToy/AllToy.jsx
+++ src/Pages/AllToy/AllToy.jsx
@@ -1,13 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+import { Table } from 'flowbite-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import AllToyTable from './AllToyTable';
 
 function AllToy() {
     const toys = useLoaderData();
     console.log(toys);
     return (
-        <div>
-            hel
-            <h1>okk</h1>
+        <div className="mt-10 w-full overflow-x-auto">
+            <Table className="">
+                <Table.Head>
+                    <Table.HeadCell className="text-base">Toy name</Table.HeadCell>
+                    <Table.HeadCell className="text-base ">Seller</Table.HeadCell>
+                    <Table.HeadCell className="text-base ">Sub-Category</Table.HeadCell>
+                    <Table.HeadCell className="text-base ">Price</Table.HeadCell>
+                    <Table.HeadCell className="text-base ">Stock Qty</Table.HeadCell>
+                    <Table.HeadCell className="text-base ">View Details</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                    {toys.slice(0, 20)?.map((toy) => (
+                        <AllToyTable key={toy._id} toys={toy} />
+                    ))}
+                </Table.Body>
+            </Table>
         </div>
     );
 }
---
+++ src/Pages/AllToy/AllToyTable.jsx
@@ -0,0 +1,29 @@
+import { Table } from 'flowbite-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function AllToyTable({ toys }) {
+    const { name, category, sellerName, price, qty, _id } = toys;
+    return (
+        <Table.Row className="bg-white  dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell>
+                <h3 className="text-lg text-primary-text">{name}</h3>
+            </Table.Cell>
+            <Table.Cell className="text-base text-secondary-text  ">{sellerName}</Table.Cell>
+            <Table.Cell className="text-base text-secondary-text  ">{category}</Table.Cell>
+            <Table.Cell className="text-base text-secondary-text  ">${price}</Table.Cell>
+
+            <Table.Cell className="text-base text-secondary-text   ">{qty}</Table.Cell>
+
+            <Table.Cell className="">
+                <Link to={`/update-toy/${_id}`}>
+                    <button className=" text-primary h-8 px-4  hover:underline text-base">
+                        View Details
+                    </button>
+                </Link>
+            </Table.Cell>
+        </Table.Row>
+    );
+}
+
+export default AllToyTable;
